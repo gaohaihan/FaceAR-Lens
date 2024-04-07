3b38b5fa47f1c853280c36371df4aaadcefc70e0
missyfang <57602734+missyfang@users.noreply.github.com>
2024-04-07 11:19:46 -0500

    Create working copy of lens basic

---
+++ lens_basic_working_copy/Public/ExpressionLinker.js
@@ -0,0 +1,44 @@
+// -----JS CODE-----
+// @input Component.FaceMaskVisual target
+// @input Component.RenderMeshVisual faceMesh
+// @input string expression
+// @input float sensitivity = 0.5
+// @input Component.ScriptComponent sliderScript
+// @input SceneObject slider 
+
+var weight = script.faceMesh.mesh.control.getExpressionWeightByName(script.expression)
+color = script.target.getMaterial(0).getPass(0).baseColor;
+updateVisual(script.target, weight)
+
+// Set sensitvity to value of slider when slider value is changed. 
+script.api.onValueChange = function(){
+   var sliderValue = script.sliderScript.api.getSliderValue();
+    script.sensitivity = sliderValue;
+}
+
+// Set sensitvity to value of slider when slider value is changed. 
+script.api.sliderToggle = function(){
+   print("slider toggle")
+    script.slider.enabled = !script.slider.enabled;
+}
+
+// Update opacity of mask based on epxression weight
+function updateVisual(visualComponent, weight) {
+    alpha = GetWeight();
+    color = visualComponent.getMaterial(0).getPass(0).baseColor;
+    visualComponent.getMaterial(0).getPass(0).baseColor = new vec4(color.r, color.g, color.b, alpha);
+}
+
+// Adjust expression weight for sensitivity
+function GetWeight(){
+    var adjusted_weight;
+    var weight = script.faceMesh.mesh.control.getExpressionWeightByName(script.expression)
+    
+    // TODO: I think this is correct but may need some adjustments
+    // particularlly to the alpha value.
+    var max_weight = 1 - script.sensitivity
+    var adjusted_weight = weight / max_weight
+    print(adjusted_weight)
+
+    return adjusted_weight 
+}
