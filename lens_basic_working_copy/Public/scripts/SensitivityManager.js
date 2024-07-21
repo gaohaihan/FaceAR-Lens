24574011ea879fb53318f75df1f7e722899b0276
missyfang <57602734+missyfang@users.noreply.github.com>
2024-07-21 13:23:29 -0500

    Link lens scripts to shared scripts folder

---
+++ lens_basic_working_copy/Public/scripts/SensitivityManager.js
@@ -0,0 +1,47 @@
+// -----JS CODE-----
+// @input Component.ScriptComponent sliderScript
+// @input Component.RenderMeshVisual faceMesh
+
+const pubSub = require("./PubSubModule");
+global.Sensitivity = 0.5;
+global.ExpressionMinValues = {};
+
+script.api.SensitivitySlider = function(){
+    SetSensitivity();
+ }
+
+  /***
+  * Toggle setting controls visibility
+  */
+ script.api.SettingsToggle = function(){
+      script.sensitivityUI.enabled = !script.sensitivityUI.enabled;
+      script.bilateralUI.enabled = !script.bilateralUI.enabled;
+  }
+
+  /***
+  * Set value of global sensitivity to value of slider when changed.
+  */
+  function SetSensitivity(){
+    var sliderValue = script.sliderScript.api.getSliderValue();
+    // Dont allow to go to 1 bc then the slider says inf and cannot be displayed. 
+    if (sliderValue > 0.9){
+      sliderValue = 0.9
+    }
+    global.Sensitivity = sliderValue;
+ }
+
+// TODO use to determine user resting expression values.
+  /***
+  * get the resting value for all expressions
+  */
+ function GetExpressionMinValues(){
+   let expressionsInSequence = Object.values(pubSub.EXPRESSIONS)
+    expressionsInSequence.forEach(element => {
+      ExpressionMinValues[element] = script.faceMesh.mesh.control.getExpressionWeightByName(element);
+   });
+  // print(ExpressionMinValues[pubSub.EXPRESSIONS.BrowsUpCenter]);
+ }
+
+pubSub.subscribe(pubSub.EVENTS.StartButtonClicked,
+   GetExpressionMinValues
+);
