1265c29f15d9d981f520a4007dea10b3cef85668
missyfang <57602734+missyfang@users.noreply.github.com>
2025-05-18 15:01:13 -0500

    remove unnecessary scripts.

--- lens_basic_migration_WIP/Assets/scripts/Converted Scripts/DisplaySensitivityInPopUp_UpdateEvent.js
+++
@@ -1,2 +0,0 @@
-// @input Component.Text DifficultyText
-script.createEvent("UpdateEvent").bind(function() { require("DisplaySensitivityInPopUp_wrapped")(script)})
--- lens_basic_migration_WIP/Assets/scripts/Converted Scripts/ExpressionTextManager_UpdateEvent.js
+++
@@ -1,6 +0,0 @@
-// @input Component.Text displayExpression
-// @input Component.Text displaySetCount
-// @input Component.Text displayRequiredSetCount
-// @input Component.Text displayRepCount
-// @input Component.Text displayRequiredRepCount
-script.createEvent("UpdateEvent").bind(function() { require("ExpressionTextManager_wrapped")(script)})
