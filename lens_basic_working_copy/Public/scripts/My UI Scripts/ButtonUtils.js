1c86ef7023b21dbc15984fa911a89632cdf3a179
missyfang <57602734+missyfang@users.noreply.github.com>
2025-05-18 15:02:37 -0500

    delete old basic project

--- lens_basic_working_copy/Public/scripts/My UI Scripts/ButtonUtils.js
+++
@@ -1,8 +0,0 @@
-// -----JS CODE-----
-// Set difficulty to value of slider when slider value is changed.
-// @input SceneObject button
-// @input SceneObject self
-
-script.api.ToggleButtons = function(){
-    script.button.enabled = !script.button.enabled;
-}
