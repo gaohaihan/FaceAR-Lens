8ec7ca1e648a39491417b8d325c19915eadf424a
missyfang <57602734+missyfang@users.noreply.github.com>
2025-08-06 21:05:10 -0500

    save scenes

---
+++ lens_brow_and_eye_exercise/Assets/scripts/Exercise Scripts/ExpressionController_Bilateral 2.js
@@ -0,0 +1,332 @@
+// -----JS CODE-----
+// @input Component.FaceMaskVisual target
+// @input Component.RenderMeshVisual faceMesh
+// @input string expressionRight
+// @input string expressionLeft
+// @input string displayText
+// @input string finishText
+// @input number completedSets
+// @input number completedReps
+// @input number baseDifficulty
+// @input number expressionIndex
+// @input Component.ScriptComponent apiScript
+
+const pubSub = require("../PubSubModule");
+
+global.requiredSets = 3;
+global.requiredReps = 5;
+
+var midRep;
+var color;
+var difficulty;
+var leftBaseExpressionValue = 0;
+var rightBaseExpressionValue = 0;
+var isRightDetectionOn;
+var isLeftDetectionOn;
+var currentDifficulty = 0;
+// face mask visual disabled by default
+script.target.enabled = false;
+
+/***
+* Called once when onAwake
+*/
+function InitializeUserBaseExpressionValue() {
+
+  var functionsToCallAfterDelay = [Initialize, BindFunctionToRunEveryUpdate, UnPause]
+
+  pubSub.publish(pubSub.EVENTS.Pause);
+
+  StartDelay(3, functionsToCallAfterDelay);
+  GetBaseExpressionValue();
+
+   function UnPause(){
+    pubSub.publish(pubSub.EVENTS.UnPause)
+  }
+}
+
+function Initialize(){
+  // Set initial values
+  currentDifficulty = (leftBaseExpressionValue + rightBaseExpressionValue) / 2 + 0.01;
+  midRep = false;
+  color = script.target.getMaterial(0).getPass(0).baseColor;
+  difficulty = global.Difficulty;
+  SetBilateralDetection();
+
+  print("init currentDifficulty : " + currentDifficulty)
+  print("init difficulty : " + difficulty)
+
+  // Display prompt text
+  pubSub.publish(pubSub.EVENTS.SetExpressionRequiredSetText,  global.requiredSets.toString());
+  pubSub.publish(pubSub.EVENTS.SetExpressionRequiredRepText,  global.requiredReps.toString());
+  pubSub.publish(pubSub.EVENTS.SetExpressionPromptText, script.displayText);
+}
+
+/***
+* Set functions to be called every frame
+*/
+function BindFunctionToRunEveryUpdate() {
+  var updateEvent = script.createEvent("UpdateEvent");
+  updateEvent.bind(OnUpdate);
+}
+
+/***
+* Grab user base expression values
+*/
+function GetBaseExpressionValue() {
+  pubSub.publish(pubSub.EVENTS.SetExpressionPromptText, "Initializing, please not move for 3s");
+  leftBaseExpressionValue = GetRawLeftWeight();
+  print("left init" + leftBaseExpressionValue.toString())
+  rightBaseExpressionValue = GetRawRightWeight();
+  print("right init" + rightBaseExpressionValue.toString())
+}
+
+/***
+* Start with a delay and invoke methods in list after delay complete
+*/
+function StartDelay(seconds, functionList){
+  var delayedEvent = script.createEvent("DelayedCallbackEvent");
+  delayedEvent.bind(function(eventData)
+  {
+   executeFunctions(eventData, functionList);
+  });
+  delayedEvent.reset(seconds);
+
+}
+
+/**
+* function that executes all given functions
+*/
+function executeFunctions(eventData, functions) {
+ functions.forEach(func => func(eventData));
+}
+
+
+/***
+* Things to be called every frame
+*/
+// implememt some sort of pause to pause the detection / game while we re do base expression initalization.
+// global value pause controlled by pubsub pause and unpause , like difficutly
+//
+function OnUpdate(){
+  difficulty = global.Difficulty;
+
+  if (global.Pause == true)
+    return;
+
+  CountReps();
+  UpdateCurrentDifficulty();
+  UpdateVisual(script.target);
+  DetermineJump();
+}
+
+/***
+* Update opacity of mask based on expression weight
+*/
+function UpdateVisual(visualComponent) {
+  alpha = GetRawExpressionWeight();
+  color = visualComponent.getMaterial(0).getPass(0).baseColor;
+  visualComponent.getMaterial(0).getPass(0).baseColor = new vec4(color.r, color.g, color.b, alpha);
+}
+
+/***
+* Set the current minimum value needed to count an expression display
+*/
+function UpdateCurrentDifficulty(){
+  var currentMinDifficulty;
+ 
+  if (isLeftDetectionOn && isRightDetectionOn ){
+    currentMinDifficulty = ((leftBaseExpressionValue + rightBaseExpressionValue)/2) + 0.01
+  }
+
+  if (!isRightDetectionOn){
+    currentMinDifficulty = rightBaseExpressionValue + 0.01
+  }
+
+  if (!isLeftDetectionOn){
+    currentMinDifficulty = leftBaseExpressionValue + 0.01
+  }
+
+  currentDifficulty = currentMinDifficulty / ( 1 - difficulty);
+
+  print("update currentDifficulty : " + currentDifficulty)
+  print("update difficulty : " + difficulty)
+
+  // cannot be detected over 1
+  if (currentDifficulty > 1)
+    currentDifficulty = 1;
+}
+
+/***
+* Count completed reps, expression must return to base line bf another rep is counted.
+*/
+function CountReps() {
+  // Update sets and reps text during exercise if changed
+  pubSub.publish(pubSub.EVENTS.SetExpressionRequiredSetText,  global.requiredSets.toString());
+  pubSub.publish(pubSub.EVENTS.SetExpressionRequiredRepText,  global.requiredReps.toString());
+
+    //stop counting when hit required sets
+    if (script.completedSets >= global.requiredSets && script.completedSets >= global.requiredSets){
+        Finished();
+        return;
+    }
+
+    // Update rep count text
+    pubSub.publish(pubSub.EVENTS.SetExpressionSetText,  script.completedSets.toString() );
+    pubSub.publish(pubSub.EVENTS.SetExpressionRepText,  script.completedReps.toString());
+
+    var rawWeight = GetRawExpressionWeight();
+    if (rawWeight > currentDifficulty && midRep !== true){
+      midRep = true;
+      script.completedReps += 1
+      //script.apiScript.sendDataToSite('completedReps', script.completedReps);
+      if (script.completedReps >= global.requiredReps){
+          script.completedSets += 1;
+          script.completedReps = 0;
+      }
+      pubSub.publish(pubSub.EVENTS.SetExpressionSetText,  script.completedSets.toString() );
+      pubSub.publish(pubSub.EVENTS.SetExpressionRepText,  script.completedReps.toString());
+    }
+
+    var rawWeight = GetRawExpressionWeight();
+    if (rawWeight <= currentDifficulty && midRep === true){
+      midRep = false;
+    }
+ }
+
+/***
+* Publish expressions values for bilateral detection so the UI reflects its values.
+*/
+function SetBilateralDetection() {
+  // set values to true for first time.
+  if (isLeftDetectionOn == null)
+    isLeftDetectionOn = true;
+  if (isRightDetectionOn == null)
+    isRightDetectionOn = true;
+
+  // enable bilateral controls
+  pubSub.publish(pubSub.EVENTS.SetBilateralDetection, true);
+  // set right/left buttons on/off
+  pubSub.publish(pubSub.EVENTS.SetBilateralDetection_Left, isLeftDetectionOn);
+  pubSub.publish(pubSub.EVENTS.SetBilateralDetection_Right, isRightDetectionOn);
+ }
+
+/**
+* Gets the raw expression weight for both or one side of the expression.
+* Note that the expression weights are flipped.
+* i.e left = on, return right weight.
+*/
+function GetRawExpressionWeight(){
+  var leftWeight = GetRawLeftWeight();
+  var rightWeight = GetRawRightWeight();
+  var combinedWeight = (leftWeight + rightWeight) / 2
+  DisplayDebug(leftWeight, rightWeight, combinedWeight)
+
+  if (!isLeftDetectionOn && !isRightDetectionOn ){
+    print("an error has occurred and both left and right side detection is off for expression")
+  }
+
+  if (!isRightDetectionOn){
+    return rightWeight;
+  }
+
+  if (!isLeftDetectionOn){
+    return leftWeight;
+  }
+
+  return combinedWeight
+}
+
+function GetRawLeftWeight(){
+  return script.faceMesh.mesh.control.getExpressionWeightByName(script.expressionLeft);
+}
+
+function GetRawRightWeight(){
+  return  script.faceMesh.mesh.control.getExpressionWeightByName(script.expressionRight);
+}
+/**
+ * Display finished text
+ */
+function Finished(){
+  if (script.completedSets >= script.requiredSets && script.completedSets >= script.requiredSets){
+    pubSub.publish(pubSub.EVENTS.SetExpressionPromptText, script.finishText);
+  }
+}
+
+/**
+ * Display value for debugging
+ */
+function DisplayDebug(leftWeight, rightWeight, combinedWeight){
+  if (!isRightDetectionOn){
+    leftWeight = 0
+  }
+
+  if (!isLeftDetectionOn){
+    rightWeight = 0
+  }
+
+  pubSub.publish(pubSub.EVENTS.SetMinExpressionWeightText,  currentDifficulty.toFixed(3).toString());
+  pubSub.publish(pubSub.EVENTS.SetCombinedText, combinedWeight.toFixed(3).toString());
+  pubSub.publish(pubSub.EVENTS.SetLeftDebugText, rightWeight.toFixed(3).toString());
+  pubSub.publish(pubSub.EVENTS.SetRightDebugText, leftWeight.toFixed(3).toString());
+
+}
+
+/**
+ * Calculate jump amount based on sensitivity
+ * Send jump to sphere controller
+ */
+function DetermineJump(){
+  var weight = GetRawExpressionWeight();
+  //  Listened to by sphereController
+  pubSub.publish(pubSub.EVENTS.SetJumpAmount, weight);
+}
+
+
+/*SUBSCRIPTIONS*/
+
+/***
+* Enable this script/exercise if the parameter matched the expressionIndex value. 
+* Initialize values
+* Disable/enable visuals
+* Start detecting on frame update
+* Always set reps back to 0 when leave a exercise
+*/
+pubSub.subscribe(pubSub.EVENTS.ExpressionIndexEnabled, (data) => {
+  if(data == script.expressionIndex)
+  {
+    script.enabled = true;
+    script.target.enabled = true;
+    script.completedSets = 0;
+    script.completedReps = 0;
+    pubSub.publish(pubSub.EVENTS.SetExpressionSetText, script.completedSets.toString());
+    pubSub.publish(pubSub.EVENTS.SetExpressionRepText, script.completedReps.toString());
+    InitializeUserBaseExpressionValue();
+  }
+  else
+  {
+    script.enabled = false;
+    script.target.enabled = false;
+  }
+});
+
+/***
+* Determine if should detect left side movement based on UI buttons being toggled
+*/
+pubSub.subscribe(pubSub.EVENTS.ToggleBilateralDetection_Left, (data) => {
+  isLeftDetectionOn = data
+});
+
+/***
+* Determine if should detect right side movement based on UI buttons being toggled
+*/
+pubSub.subscribe(pubSub.EVENTS.ToggleBilateralDetection_Right, (data) => {
+  isRightDetectionOn = data
+});
+
+
+/**
+ * Pause exercise and reinit base expression value.
+ */
+pubSub.subscribe(pubSub.EVENTS.ReInitializeBaseExpression, () => {
+  InitializeUserBaseExpressionValue();
+});
---
+++ lens_brow_and_eye_exercise/Assets/scripts/My UI Scripts/SettingsUiManager 2.js
@@ -0,0 +1,123 @@
+// -----JS CODE-----
+// @input SceneObject difficultyUI
+// @input SceneObject countUI
+// @input SceneObject controlsUI
+// @input SceneObject bilateralUI
+// @input SceneObject debugUI
+// @input Component.ScriptComponent bilateralToggle_left
+// @input Component.ScriptComponent bilateralToggle_right
+
+const pubSub = require("../PubSubModule");
+script.ToggleOn_Left = ToggleOnLeft;
+script.ToggleOn_Right = ToggleOnRight;
+script.ToggleOff_Left = ToggleOffLeft
+script.ToggleOff_Right = ToggleOffRight;
+script.ToggleUI = ToggleUI;
+script.ToggleDebugUI = ToggleDebugUI;
+
+/***
+ * Toggle UI
+ */
+function ToggleUI(){
+    print("button hit")
+    print(script.difficultyUI.enabled);
+    script.difficultyUI.enabled = !script.difficultyUI.enabled;
+    script.controlsUI.enabled = !script.controlsUI.enabled;
+    script.countUI.enabled = !script.countUI.enabled;
+}
+
+/***
+ * Toggle debug UI
+ */
+function ToggleDebugUI(){
+    script.debugUI.enabled = !script.debugUI.enabled;
+}
+/***
+ * Publish bilateral controls being toggle on/off
+ */
+function ToggleDebug(){
+    print("2")
+    pubSub.publish(pubSub.EVENTS.ToggleBilateralDetection_Left, true);
+    script.bilateralToggle_left.toggleOn();
+}
+/***
+ * Publish bilateral controls being toggle on/off
+ */
+function ToggleOnLeft(){
+    print("left on")
+    pubSub.publish(pubSub.EVENTS.ToggleBilateralDetection_Left, true);
+    script.bilateralToggle_left.toggleOn();
+}
+
+function ToggleOnRight(){
+    print("right on")
+    pubSub.publish(pubSub.EVENTS.ToggleBilateralDetection_Right, true);
+    script.bilateralToggle_right.toggleOn();
+}
+
+function ToggleOffLeft (){
+    print("left off")
+    pubSub.publish(pubSub.EVENTS.ToggleBilateralDetection_Left, false);
+    script.bilateralToggle_left.toggleOff();
+    print("toggle off left should turn on right")
+    // cannot turn of both side, if off right side detection is turned on
+    if (!script.bilateralToggle_right.getToggleValue())
+        print("toggle of left turn on right")
+        ToggleOnRight();
+}
+
+function ToggleOffRight(){
+    print("right off")
+    pubSub.publish(pubSub.EVENTS.ToggleBilateralDetection_Right, false);
+    script.bilateralToggle_right.toggleOff();
+    print("toggle of right should turn on left")
+    // cannot turn of both side, if off left side detection is turned on
+    if (!script.bilateralToggle_left.getToggleValue())
+        print("toggle of right turn on left")
+        ToggleOnLeft();
+}
+
+/***
+  * Set left button on/off based on data
+  */
+pubSub.subscribe(pubSub.EVENTS.SetBilateralDetection_Left, (data) => {
+    if (data === true)
+        print("left on")
+        script.bilateralToggle_left.toggleOn();
+    if (data === false)
+        print("left off")
+        script.bilateralToggle_left.toggleOff();
+  });
+
+/***
+* Set right button on/off based on data
+*/
+pubSub.subscribe(pubSub.EVENTS.SetBilateralDetection_Right, (data) => {
+if (data === true)
+    print("right on")
+    script.bilateralToggle_right.toggleOn();
+if (data === false)
+    print("right off")
+    script.bilateralToggle_right.toggleOff();
+});
+
+/***
+  * Set left button on/off based on data
+  */
+pubSub.subscribe(pubSub.EVENTS.SetBilateralDetection, (data) => {
+    if (data === true){
+        script.bilateralToggle_left.enabled = true;
+        script.bilateralToggle_right.enabled = true;
+        // script.bilateralToggle_left.enableInteractable();
+        // script.bilateralToggle_right.enableInteractable();
+    }
+
+    if (data === false){
+        // script.bilateralToggle_left.getComponent("ButtonComponent").enabled = false;
+        // script.bilateralToggle_right.getComponent("ButtonComponent").enabled = false;
+        script.bilateralToggle_left.enabled = false;
+        script.bilateralToggle_right.enabled = false;
+    }
+
+  });
+
